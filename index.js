98417b9c1848883a34eb782780a956d0ce6d0884
whawari <walid.hawari@imaginelabs.me>
2020-12-14 18:06:25 +0200

    updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ 
+      hi: "there", 
+      done_by: {
+          1: "Walid Hawari",
+          2: "Ibrahim Jardani"
+      } 
+    });
 });
 
 const PORT = process.env.PORT || 5000;
