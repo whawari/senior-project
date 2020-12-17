c5b43d6d9a5ae9acfec02b1ea64441df4896e9d0
whawari <walid.hawari@imaginelabs.me>
2020-12-17 17:39:37 +0200

    generated react-app

---
+++ client/src/setupProxy.js
@@ -0,0 +1,10 @@
+// restart the server every time you change this file
+const { createProxyMiddleware } = require("http-proxy-middleware");
+module.exports = function (app) {
+  app.use(
+    ["/api", "/auth/google"],
+    createProxyMiddleware({
+      target: "http://localhost:5000",
+    })
+  );
+};
